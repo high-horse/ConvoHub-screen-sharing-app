622f13d1916d6a00b1314dd61387ac28e96c8720
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-08-12 21:07:39 +0545

    filter self

--- vue-client/src/components/ClientsList.vue
+++ vue-client/src/components/ClientsList.vue
@@ -3,7 +3,7 @@
     <div>
         <ul>
             <li v-for="client in clientsList" :key="client">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button v-if="client != myWsID"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                    {{ client }}
                 </button>
             </li>
